6e9facd289fa5d20187755428028332ed7022f9f
2005jan <hafenjan@gmail.com>
2024-03-14 23:33:49 +0100

    endless runner added

--- Hangman.py
+++ Hangman.py
@@ -1,9 +1,9 @@
 #############################
 #       author: Jan         #
 #       date: 07.03.2024    #
+# brief: Hangman in German  #
 #############################
 
-#Game Hangman
 
 #############################
 #       imports             #
@@ -14,16 +14,20 @@ import random
 #############################
 #       variables           #
 #############################
-
-from word import word       #variable word is assigned with words from word.py
-word=random.choice(word)    # chooses random word out of list
-print(word)
-guessed_letters=[]   
-lives=6             
+score=0
+next_round=True         
 
 #############################
 #       functions           #
 #############################
+def init():
+    from word import word       #variable word is assigned with words from word.py
+    random_word=random.choice(word)    # chooses random word out of list
+    print(random_word)
+    return random_word
+guessed_letters=[]   
+lives=6
+
 def print_word():
     for i in word:
         if i in guessed_letters:
@@ -31,21 +35,28 @@ def print_word():
         else:
             print("_", end=" ")
     print(f"\nSie haben noch {lives} Leben")
+
 def user_guess(guess):
+    global lives,score,next_round
     if guess not in word:
-            global lives
             lives -=1
+            score-=1
+    else: 
+        score+=1
     print_word()
     print(f"Sie haben bisher die Buchstaben {guessed_letters} versucht.")
+    print(f"Punktzahl: {score}")
     if all(letter in guessed_letters for letter in set(word)):
         print("Glückwunsch! Sie haben das Wort erraten.")
         return 
     elif lives==0:
         print(f"Sie haben keine Leben mehr! Das Wort war {word}.")
+        next_round=False
 
             
 def user_input():
-    while lives and not all(letter in guessed_letters for letter in (word)):
+    global lives,next_round
+    while lives>0 and not all(letter in guessed_letters for letter in (word)):
         input_guess=input("\nGeben Sie einen Buchstaben ein: ")   
         input_guess=input_guess.upper()     #capitalizes user input
         alphabet=input_guess.isalpha()      #alphabet is only True when the input is in the alphabet
@@ -54,14 +65,18 @@ def user_input():
         elif len(input_guess) == 1 and alphabet==True:    #checks if the input is only one letter
             guessed_letters.append(input_guess)     #adds guess to guessed_letters
             user_guess(input_guess)
+        elif input_guess=="QUIT":
+            next_round=False
+            lives=0
         else:
-            print("Falsche Eingabe! Bitte geben Sie nur einen Buchstaben ein.")  
+            print("Falsche Eingabe! Bitte geben Sie einen Buchstaben ein.")  
 #############################
 #       main                #
 #############################
-
-print_word()
-user_input()
+while next_round==True:
+    word=init()
+    print_word()
+    user_input()
 #############################
 #       result              #
 #############################
