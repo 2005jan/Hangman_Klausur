8607b4c9c45100863a5ae3fa802efcff0b069a1b
2005jan <hafenjan@gmail.com>
2024-03-14 22:37:48 +0100

    win and loose conditions added

--- Hangman.py
+++ Hangman.py
@@ -15,54 +15,53 @@ import random
 #       variables           #
 #############################
 
-from word import word  #variable word is assigned with words from word.py
+from word import word       #variable word is assigned with words from word.py
 word=random.choice(word)    # chooses random word out of list
 print(word)
 guessed_letters=[]   
-lives=6             #temp
+lives=6             
 
 #############################
 #       functions           #
 #############################
 def print_word():
-    for letters in word:    #prints "_" for every letter in word
-        print("",end="_")
-
+    for i in word:
+        if i in guessed_letters:
+            print(i, end=" ")
+        else:
+            print("_", end=" ")
+    print(f"\nSie haben noch {lives} Leben")
 def user_guess(guess):
     if guess not in word:
             global lives
-            lives = lives-1
-            print("",end="")
-    for letter in word:
-            if guess==letter:    
-                print("TRUE")
+            lives -=1
+    print_word()
     print(f"Sie haben bisher die Buchstaben {guessed_letters} versucht.")
     if all(letter in guessed_letters for letter in set(word)):
-        print("won")
+        print("Glückwunsch! Sie haben das Wort erraten.")
         return 
+    elif lives==0:
+        print(f"Sie haben keine Leben mehr! Das Wort war {word}.")
+
             
 def user_input():
-    while lives>0 and not all(letter in guessed_letters for letter in set(word)):
-        while True:
-            input_guess=input("\nGeben Sie einen Buchstaben ein: ")   #user inputs a letter
-            input_guess=input_guess.upper()     #capitalizes user input
-            alphabet=input_guess.isalpha()      #alphabet is only True when the input is in the alphabet
-            if input_guess in guessed_letters:    #checks if this letter was already guessed before
-                print("Diesen Buchstaben haben Sie bereits versucht.")
-            elif len(input_guess) == 1 and alphabet==True:    #checks if the input is only one letter
-                guessed_letters.append(input_guess)     #adds guess to guessed_letters
-                user_guess(input_guess)
-            else:
-                 print("Falsche Eingabe! Bitte geben Sie nur einen Buchstaben ein.")
-
+    while lives and not all(letter in guessed_letters for letter in (word)):
+        input_guess=input("\nGeben Sie einen Buchstaben ein: ")   
+        input_guess=input_guess.upper()     #capitalizes user input
+        alphabet=input_guess.isalpha()      #alphabet is only True when the input is in the alphabet
+        if input_guess in guessed_letters:  #checks if this letter was already guessed before
+            print("Diesen Buchstaben haben Sie bereits versucht.")
+        elif len(input_guess) == 1 and alphabet==True:    #checks if the input is only one letter
+            guessed_letters.append(input_guess)     #adds guess to guessed_letters
+            user_guess(input_guess)
+        else:
+            print("Falsche Eingabe! Bitte geben Sie nur einen Buchstaben ein.")  
 #############################
 #       main                #
 #############################
 
 print_word()
 user_input()
-if lives==0:
-    print("lost")
 #############################
 #       result              #
 #############################
