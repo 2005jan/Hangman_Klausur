07cf42e9bd800bcd4be09131d0e3d10cae25200d
2005jan <hafenjan@gmail.com>
2024-03-14 18:20:09 +0100

    added functions and shows if user won or lost

--- Hangman.py
+++ Hangman.py
@@ -24,37 +24,41 @@ lives=6             #temp
 #############################
 #       functions           #
 #############################
+def print_word():
+    for letters in word:    #prints "_" for every letter in word
+        print("",end="_")
 
-for letters in word:    #prints "_" for every letter in word
-    print("",end="_")
-
-def user_input():
-    while lives>0:
-        guess=input("\nGeben Sie einen Buchstaben ein: ")   #user inputs a letter
-        guess=guess.upper()     #capitalizes user input
-        if guess in guessed_letters:    #checks if this letter was already guessed before
-            print("Double input")
-        else:
-            guessed_letters.append(guess)
-        for letter in word:
-            if guess==letter:
+def user_guess(guess):
+    if guess in guessed_letters:    #checks if this letter was already guessed before
+        print("Double input")
+    else:
+        guessed_letters.append(guess)
+        if guess not in word:
+            global lives
+            lives = lives-1
+            print("",end="")
+    for letter in word:
+            if guess==letter:    
                 print("TRUE")
-            else:    
-                lives -=1
-                print("",end="")
-        print(f"Letters guessed till now:{guessed_letters}")
-        if all(letter in guessed_letters for letter in set(word)):
-            print("Won")
-            return
+    print(f"Letters guessed till now:{guessed_letters}")
+    if all(letter in guessed_letters for letter in set(word)):
+        print("won")
+        return 
             
-
+def user_input():
+    while lives>0 and not all(letter in guessed_letters for letter in set(word)):
+        input_guess=input("\nGeben Sie einen Buchstaben ein: ")   #user inputs a letter
+        input_guess=input_guess.upper()     #capitalizes user input
+        user_guess(input_guess)
 
 #############################
 #       main                #
 #############################
 
+print()
 user_input()
-
+if lives==0:
+    print("lost")
 #############################
 #       result              #
 #############################
